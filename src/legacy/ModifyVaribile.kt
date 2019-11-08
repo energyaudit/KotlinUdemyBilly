54c0fce547f3a54c83f530b690836fef9ead5781
hyangloyaltycom <hyang2@loyalty.com>
2019-11-08 17:06:33 -0500

    add orderoperation etc

---
+++ src/legacy/ModifyVaribile.kt
@@ -0,0 +1,17 @@
+package legacy
+
+fun main(){
+
+    var count =1
+    println("Count: $count")
+
+    count += 1
+    println("Count: $count")
+
+    count += 3
+    println("Count: $count")
+
+    count -= 2
+    println("Count: $count")
+
+}
